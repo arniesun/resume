9517d268916de79639ae3f4dd0ec9176bf1c0529
arniesun <115664815+arniesun@users.noreply.github.com>
2022-10-14 02:23:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-function revealMessage() { 
-  document.getElementByID("hiddenMessage").style.display = 'block';
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read More";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read Less";
+    moreText.style.display = "inline";
+  }
 }
